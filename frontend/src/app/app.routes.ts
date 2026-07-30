796cdbcf0ac394845cc2516fe6f93055d495074e
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2026-07-29 19:04:38 -0500

    feat: set up Angular frontend with login route

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('@pages').then((m) => m.Login),
+  },
+//   {
+//     path: 'home',
+//     loadComponent: () => import('@pages').then((m) => m.Home),
+//   },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
